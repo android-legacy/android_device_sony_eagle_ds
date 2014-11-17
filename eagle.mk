0cd57eaabcd324d817ae6e8c48bd623d370b4348
erikcas <erikcas1972@gmail.com>
2014-11-17 19:33:58 +0100

    donot copy audio_effects
    it is set by DSP manager
    
    Change-Id: I90b879cb36f7a1f60d445b95720ca947baec2141

--- eagle.mk
+++ eagle.mk
@@ -69,7 +69,6 @@ PRODUCT_COPY_FILES += \
 
 # Audio Configs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/rootdir/system/etc/mixer_paths_VY52.xml:system/etc/mixer_paths_VY52.xml \
